7a4830c5d7b8676723bdb98f8c35747a7dabfd71
brucewong21 <brucewong21@gmail.com>
2018-05-10 20:00:39 -0700

    integrate redux form

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
+
+let store = createStore(rootReducer);
+
+ReactDOM.render(
+	<Provider store={ store }>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
+
 registerServiceWorker();
