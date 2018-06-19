95b8ae7e234d317a76901bd51d782ad6305ca4fd
Brandon Him <brandonhim@live.com>
2018-06-18 20:50:50 -0700

    Add result page

--- src/components/SingleStatement.jsx
+++ src/components/SingleStatement.jsx
@@ -3,7 +3,7 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 
 
-function SingleStatement({handleSubmit, heading, position, forwardPositions, businessType, address}) {
+function SingleStatement({handleSubmit, heading, position, forwardPositions, address, ...props}) {
 
   // If there are variables in the heading
   const variablesMatched = heading.match(/#{(\w+)}/g);
@@ -15,12 +15,41 @@ function SingleStatement({handleSubmit, heading, position, forwardPositions, bus
       heading = heading.replace(regExp, variable);
     });
   }
-
+  const { 
+    zoningDescription,
+    zoningAbbreviation,
+    businessName,
+    businessAddress,
+    businessType
+  } = props;
   return (
-    <form onSubmit={(e) => {handleSubmit(forwardPositions[0], e)}}>
-      <h1>{ heading }</h1>
-      <button>Submit</button>
-    </form>    
+    <div className="results__main">
+      <form onSubmit={(e) => {handleSubmit(forwardPositions[0], e)}}>
+        <div className="card">
+          <h3>Are you Permitted?</h3>
+          <p>{ heading }</p>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <div className="results__business-info card">
+              <h3>Business</h3>
+              <p><b>Name: </b>{ businessName }</p>
+              <p><b>Type: </b>{ businessType }</p>
+              <p><b>Address: </b>{ businessAddress }</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="results__zoning-info card">
+              <h3>Zoning</h3>
+              <p><b>Description: </b>{ zoningDescription }</p>
+              <p><b>Abbreviation: </b>{ zoningAbbreviation }</p>
+            </div>
+          </div>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
@@ -29,7 +58,11 @@ const mapStateToProps = state => {
     position: state.position,
     businessType: state.form.wizard.values.businessType,
     address: state.map.address,
-    forwardPositions: state.graph.adjancey[state.position]
+    forwardPositions: state.graph.adjancey[state.position],
+    zoningAbbreviation: state.map.zoningAbbreviation,
+    zoningDescription: state.map.zoningDescription,
+    businessAddress: state.map.address,
+    businessName: state.map.name
   }
 };
 
